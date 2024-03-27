8e15fa660810bd7c32eae4e752da1e61b874a82d
woonzf <137765412+woonzf@users.noreply.github.com>
2024-03-27 21:08:55 +0800

    Add insertAt(), removeAt() and clean codes

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ linkedList.prepend(10)
 linkedList.append(40)
 linkedList.append(50)
 linkedList.pop()
+linkedList.insertAt(100, 3)
+linkedList.removeAt(4)
 
-console.log(linkedList)
-console.log(linkedList.size())
-console.log(linkedList.head())
-console.log(linkedList.tail())
-console.log(linkedList.at(3))
-console.log(linkedList.contains(20))
-console.log(linkedList.find(10))
-console.log(linkedList.toString())
+console.log(linkedList.size()) // 4
+console.log(linkedList.head()) // Node { 10 }
+console.log(linkedList.tail()) // Node { 100 }
+console.log(linkedList.at(1)) // Node { 20 }
+console.log(linkedList.contains(100)) // true
+console.log(linkedList.find(100)) // 3
+console.log(linkedList.toString()) // ( 10 ) -> ( 20 ) -> ( 30 ) -> ( 100 ) -> null
--- linked-list.js
+++ linked-list.js
@@ -25,13 +25,13 @@ export default class LinkedList {
 
     size() {
         let size = 0;
-        if (this.listHead == null) return size;
-
         let current = this.listHead;
+
         while (current !== null) {
             size += 1;
             current = current.nextNode;
         }
+
         return size;
     }
 
@@ -49,56 +49,106 @@ export default class LinkedList {
 
     at(index) {
         if (index < 0) return "Index cannot be less than 0";
-        if (index >= 0 && this.listHead == null) return `No node found at index ${index}, linked list is empty`;
+        if (this.listHead == null) return `No node found, linked list is empty`;
+        
         let current = this.listHead;
+        
         for (let i = 0; i < index; i++) {
             current = current.nextNode;
-            if (i < index && current == null) return `No node found at index ${index}, max index is ${i}`;
+            if (current == null) return `No node found at index ${index}, max index is ${i}`;
         }
+
         return current;
     }
 
     pop() {
         if (this.listHead == null) return;
-        let current = this.listHead;
+
         let prev = null;
+        let current = this.listHead;
+
         while (current.nextNode !== null) {
             prev = current;
             current = current.nextNode;
         }
+
         prev.nextNode = null;
     }
 
     contains(value) {
         if (this.listHead == null) return false;
+        
         let current = this.listHead;
+
         while (current !== null) {
             if (current.value == value) return true;
-            if (current.nextNode == null) return false;
             current = current.nextNode;
         }
+
+        return false;
     }
 
     find(value) {
         if (this.listHead == null) return null;
+        
         let current = this.listHead;
         let i = 0;
+
         while (current !== null) {
             if (current.value == value) return i;
-            if (current.nextNode == null) return null;
             current = current.nextNode;
             i++;
         }
+
+        return null;
     }
 
     toString() {
         if (this.listHead == null) return "Linked list is empty";
-        let string = `( ${this.listHead.value} )`;
-        let current = this.listHead.nextNode;
+
+        let current = this.listHead;
+        let string = "";
+
         while (current !== null) {
-            string += ` -> ( ${current.value} )`;
-            if (current.nextNode == null) return string += ` -> null`;
+            string += `( ${current.value} ) -> `;
             current = current.nextNode;
+        }
+
+        return string += "null";
+    }
+
+    insertAt(value, index) {
+        if (this.listHead == null) this.append(value);
+        else {
+            let prev = null;
+            let current = this.listHead;
+
+            for (let i = 0; i < index; i++) {
+                prev = current;
+                current = current.nextNode;
+                if (current == null) break;
+            }
+
+            const newNode = new Node(value);
+            prev.nextNode = newNode;
+            newNode.nextNode = current;
+        }
+    }
+
+    removeAt(index) {
+        if (this.listHead == null) return;
+        if (index == 0) this.listHead = this.listHead.nextNode;
+        else {
+            let prev = null;
+            let current = this.listHead;
+
+            for (let i = 0; i < index; i++) {
+                prev = current;
+                current = current.nextNode;
+                if (current == null) return;
+            }
+
+            prev.nextNode = current.nextNode;
         }
     }
 }
